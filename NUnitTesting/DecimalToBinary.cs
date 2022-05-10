700afa3e0156cee1c613d62af1e0545deeb8e771
lokeshsonawane95 <lokeshsonawane95@gmail.com>
2022-05-10 18:51:52 +0530

    Added to DecimalToBinary

---
+++ NUnitTesting/DecimalToBinary.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTesting
+{
+    internal class DecimalToBinary
+    {
+        public static void ToBinary()
+        {
+            int[] binaryNumber = new int[32];
+            Console.Write("Enter a decimal number to convert into binary : ");
+            int number = Convert.ToByte(Console.ReadLine());
+            int i = 0;
+            Console.Write("Binary representation of " + number + " is : ");
+
+            //Decimal to binary conversion logic
+            while (number > 0)
+            {
+                binaryNumber[i] = number % 2;
+                number /= 2;
+                i++;
+            }
+
+            //We are printing the array in reverse to get the binary representation
+            for (int j = i - 1; j >= 0; j--)
+            {
+                Console.Write(binaryNumber[j]);
+            }
+        }
+    }
+}
--- NUnitTesting/Program.cs
+++ NUnitTesting/Program.cs
@@ -9,12 +9,14 @@ namespace NUnitTesting
             int choice;
             do
             {
-                Console.WriteLine("1. Find day of week");
+                Console.WriteLine("\n1. Find day of week");
                 Console.WriteLine("2. Temperature conversion");
                 Console.WriteLine("3. Monthly payment");
                 Console.WriteLine("4. Square root by Newton's method");
+                Console.WriteLine("5. Decimal to binary conversion");
 
                 Console.WriteLine("0. Exit");
+                Console.Write("Enter your choice : ");
                 choice = Convert.ToInt32(Console.ReadLine());
                 switch (choice)
                 {
@@ -29,6 +31,9 @@ namespace NUnitTesting
                         break;
                     case 4:
                         SquareRoot.Root();
+                        break;
+                    case 5:
+                        DecimalToBinary.ToBinary();
                         break;
                 }
             } while (choice != 0);
